f5ff0addfb7dc944a4ef1265b71b4e8d41f0d14b
leoalves <hello@leoalves.net>
2016-11-08 23:02:28 -0500

    'sendMessage'

---
+++ index.js
@@ -0,0 +1,37 @@
+'use strict'
+const AWS = require('aws-sdk')
+
+/**
+   * Converts Javascript Object into SQS Message Attributes
+   *
+   * @name convertJSObjectToSQSAttributes
+   * @function
+   * @param {object} jsObj Javascript Object that needs to be converted
+   * @throws if jsObjs is not an object
+   * TODO: Throw Error if nested Object
+   * TODO: Convert to more types than just string
+ */
+function convertJSObjectToSQSAttributes (jsObj) {
+  let sqsAttribute = {}
+  for (var prop in jsObj) {
+    if (jsObj.hasOwnProperty(prop)) {
+      sqsAttribute[prop] = {
+        DataType: 'String',
+        StringValue: jsObj[prop]
+      }
+    }
+  }
+  return sqsAttribute
+}
+
+exports.sendMessage = function (QueueUrl, Body, Attributes) {
+  const sqs = new AWS.SQS()
+
+  let params = {
+    MessageBody: Body,
+    QueueUrl: QueueUrl,
+    Attributes: convertJSObjectToSQSAttributes(Attributes)
+  }
+
+  return sqs.sendMessage(params).Promise()
+}
